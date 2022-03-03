ecb2e19c27b08a09f40e4e913e9dfcb04af5298d
timkimcool <timkimcool@gmail.com>
2022-03-02 19:41:24 -0500

    rebuild auto

--- index.js
+++ index.js
@@ -10,17 +10,6 @@ const clientId = process.env.clientId;
 const guildId = process.env.guildId;
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-// read events
-// const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-// for (const file of eventFiles) {
-// 	const event = require(`./events/${file}`);
-// 	if (event.once) {
-// 		client.once(event.name, (...args) => event.execute(...args));
-// 	} else {
-// 		client.on(event.name, (...args) => event.execute(...args));
-// 	}
-// }
-
 // get commands
 client.commands = new Collection();
 const commands = [];
